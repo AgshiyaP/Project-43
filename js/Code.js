8108ec756d89879485bda7d894c90e189c709ea9
AgshiyaP <75052385+AgshiyaP@users.noreply.github.com>
2021-06-26 17:04:14 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "TOMORROW";
+const accessCode2 = "TOWEL";
+const accessCode3 = "FIRE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R R O W O O M T", 100,50)
+    fill("lightblue")
+    text("Hint: What is always coming but never arrives? ", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("O W E L T", 700,150)
+    fill("lightblue")
+    text("Hint: What gets wetter the more it dries?", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("I E R F", 100,250)
+    fill("lightblue")
+    text("Hint:What is it that lives if it is fed, and dies if you give it a drink?", 100,270)
+    
+
+}
